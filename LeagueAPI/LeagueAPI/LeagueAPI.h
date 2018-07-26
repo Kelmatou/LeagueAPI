b1e174cbeab8d8d6b4b18dc100c788244bb681c0
Antoine CLOP <antoine.clop@epita.fr>
2018-07-26 20:24:24 +0200

    [XCODEPROJ] Creating xcode project

---
+++ LeagueAPI/LeagueAPI/LeagueAPI.h
@@ -0,0 +1,19 @@
+//
+//  LeagueAPI.h
+//  LeagueAPI
+//
+//  Created by Antoine Clop on 7/26/18.
+//  Copyright © 2018 Antoine Clop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LeagueAPI.
+FOUNDATION_EXPORT double LeagueAPIVersionNumber;
+
+//! Project version string for LeagueAPI.
+FOUNDATION_EXPORT const unsigned char LeagueAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeagueAPI/PublicHeader.h>
+
+
