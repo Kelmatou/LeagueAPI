9a44d2318db4a041aa3f24d86d33e0c3033918c7
Antoine CLOP <antoine.clop@epita.fr>
2018-07-26 21:09:35 +0200

    [REST REQUESTER] Adding REST Requester and ObjectMapper to make request to Riot's API

---
+++ LeagueAPI/LeagueAPI/Utils/ObjectMapper.swift
@@ -0,0 +1,34 @@
+//
+//  ObjectMapper.swift
+//  LeagueAPI
+//
+//  Created by Antoine Clop on 7/26/18.
+//  Copyright © 2018 Antoine Clop. All rights reserved.
+//
+
+internal class ObjectMapper {
+    
+    /**
+     Convert data into an object of type T, if T is Decodable
+     
+     - parameter data: the data to convert
+     - parameter type: the destination type
+     
+     - returns: a couple (T?, String?) where T is the result object if conversion succeed and Error is the error if available
+     */
+    internal static func convert<T: Decodable>(_ data: Data?, into type: T.Type) -> (T?, String?) {
+        if let data = data {
+            do {
+                let dataJson: T = try JSONDecoder().decode(T.self, from: data)
+                return (dataJson, nil)
+            }
+            catch {
+                let error: String = "Could not parse \(type) data from json"
+                return (nil, error)
+            }
+        }
+        else {
+            return (nil, nil)
+        }
+    }
+}
---
+++ LeagueAPI/LeagueAPI/Utils/RESTRequester.swift
@@ -0,0 +1,125 @@
+//
+//  RESTRequester.swift
+//  LeagueAPI
+//
+//  Created by Antoine Clop on 7/26/18.
+//  Copyright © 2018 Antoine Clop. All rights reserved.
+//
+
+import UIKit
+
+internal class RESTRequester {
+    
+    public enum AccessMethod: String {
+        case GET     = "GET"
+        case POST    = "POST"
+        case PUT     = "PUT"
+        case HEAD    = "HEAD"
+        case DELETE  = "DELETE"
+        case PATCH   = "PATCH"
+        case TRACE   = "TRACE"
+        case OPTIONS = "OPTIONS"
+        case CONNECT = "CONNECT"
+    }
+    
+    /**
+     Sends an HTTP request
+     
+     - parameter method: the AccessMethod
+     - parameter url: the targeted url
+     - parameter headers: a dictionary of header [Value : HttpField]
+     - parameter body: the content of the message
+     - parameter handler: allows the user to make actions just after request ended (Data, String)
+     */
+    public static func request(_ method: AccessMethod, url: String, headers: [String : String]? = nil, body: String? = nil, handler: @escaping (Data?, String?) -> Void) {
+        if let uri = URL(string: url) {
+            var request: URLRequest = URLRequest(url: uri)
+            request.httpMethod = method.rawValue
+            if let body = body {
+                request.httpBody = body.data(using: String.Encoding.utf8)
+            }
+            if let headers = headers {
+                for header in headers {
+                    request.addValue(header.value, forHTTPHeaderField: header.key)
+                }
+            }
+            let task = URLSession.shared.dataTask(with: request) {
+                (data, response, error) in
+                if let error = error {
+                    handler(data, error.localizedDescription)
+                }
+                else {
+                    handler(data, nil)
+                }
+            }
+            task.resume()
+        }
+        else {
+            handler(nil, "Invalid URL")
+        }
+    }
+    
+    /**
+     Sends an HTTP request and that will receive text
+     
+     - parameter method: the AccessMethod
+     - parameter url: the targeted url
+     - parameter headers: a dictionary of header [Value : HttpField]
+     - parameter body: the content of the message
+     - parameter handler: allows the user to make actions just after request ended (String, String)
+     */
+    public static func requestText(_ method: AccessMethod, url: String, headers: [String : String]? = nil, body: String? = nil, handler: @escaping (String?, String?) -> Void) {
+        request(method, url: url, headers: headers, body: body) {
+            (data, error) in
+            var responseDecoded: String?
+            if let data = data, error == nil {
+                responseDecoded = String(data: data, encoding: String.Encoding(rawValue: String.Encoding.utf8.rawValue))
+            }
+            handler(responseDecoded, error)
+        }
+    }
+    
+    /**
+     Sends an HTTP request and that will receive an image
+     
+     - parameter method: the AccessMethod
+     - parameter url: the targeted url
+     - parameter headers: a dictionary of header [Value : HttpField]
+     - parameter body: the content of the message
+     - parameter handler: allows the user to make actions just after request ended (UIImage, String)
+     */
+    public static func requestImage(_ method: AccessMethod, url: String, headers: [String : String]? = nil, body: String? = nil, handler: @escaping (UIImage?, String?) -> Void) {
+        request(method, url: url, headers: headers, body: body) {
+            (data, error) in
+            var responseImage: UIImage?
+            if let data = data, error == nil {
+                responseImage = UIImage(data: data)
+            }
+            handler(responseImage, error)
+        }
+    }
+    
+    /**
+     Sends an HTTP request and that will receive an object of type T
+     
+     - parameter method: the AccessMethod
+     - parameter url: the targeted url
+     - parameter headers: a dictionary of header [Value : HttpField]
+     - parameter body: the content of the message
+     - parameter asType: the type of the output object
+     - parameter handler: allows the user to make actions just after request ended (UIImage, String)
+     */
+    public static func requestObject<T: Decodable>(_ method: AccessMethod, url: String, headers: [String : String]? = nil, body: String? = nil, asType: T.Type, handler: @escaping (T?, String?) -> Void) {
+        request(method, url: url, headers: headers, body: body) {
+            (data, error) in
+            var responseObject: T?
+            if let data = data, error == nil {
+                let conversion: (T?, String?) = ObjectMapper.convert(data, into: T.self)
+                if let object = conversion.0 {
+                    responseObject = object
+                }
+            }
+            handler(responseObject, error)
+        }
+    }
+}
