e9ec4c7a5a51e7ba8298afbb131e1d128f453209
Antoine CLOP <antoine.clop@epita.fr>
2018-07-28 23:29:07 +0200

    [VERIFICATION] Adding ThirdPartyCode method class

---
+++ LeagueAPI/LeagueAPI/Models/FrameworkModels/Methods/ThirdPartyCodeMethod.swift
@@ -0,0 +1,41 @@
+//
+//  ThirdPartyCodeMethod.swift
+//  LeagueAPI
+//
+//  Created by Antoine Clop on 7/28/18.
+//  Copyright © 2018 Antoine Clop. All rights reserved.
+//
+
+import Foundation
+
+internal class ThirdPartyCodeMethod: LeagueMethod {
+    
+    public enum ThirdPartyCodeMethods {
+        case ById(id: Double)
+    }
+    
+    private var service: ServiceProxy
+    private var method: ThirdPartyCodeMethods
+    
+    public init(method: ThirdPartyCodeMethods, region: Region) {
+        self.method = method
+        self.service = ServiceProxy(for: region)
+    }
+    
+    public func getAccessMethod() -> RESTRequester.AccessMethod {
+        return .GET
+    }
+    
+    public func getMethodSignature() -> String {
+        return "Default"
+    }
+    
+    public func getMethodUrl() -> String {
+        let entrypoint: String = self.service.host
+        let commonPath: String = "https://\(entrypoint)\(MethodPaths.Platform.rawValue)/\(Version.RiotAPI)/third-party-code"
+        switch self.method {
+        case .ById(let id):
+            return "\(commonPath)/by-summoner/\(id)"
+        }
+    }
+}
