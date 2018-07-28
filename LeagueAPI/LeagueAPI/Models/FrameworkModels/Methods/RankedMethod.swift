3b2ca90bd8b29601ae2f4eb82a09dfcb093760e1
Antoine CLOP <antoine.clop@epita.fr>
2018-07-28 13:19:14 +0200

    [LEAGUE] Adding league methods

---
+++ LeagueAPI/LeagueAPI/Models/FrameworkModels/Methods/RankedMethod.swift
@@ -0,0 +1,55 @@
+//
+//  RankedMethod.swift
+//  LeagueAPI
+//
+//  Created by Antoine Clop on 7/28/18.
+//  Copyright © 2018 Antoine Clop. All rights reserved.
+//
+
+import Foundation
+
+internal class RankedMethod: LeagueMethod {
+    
+    public enum RankedMethods {
+        case ChallengerByQueue(queue: Queue)
+        case MasterByQueue(queue: Queue)
+        case LeagueById(id: String)
+        case PositionById(id: Double)
+    }
+    
+    private var service: ServiceProxy
+    private var method: RankedMethods
+    
+    public init(method: RankedMethods, region: Region) {
+        self.method = method
+        self.service = ServiceProxy(for: region)
+    }
+    
+    func getAccessMethod() -> RESTRequester.AccessMethod {
+        return .GET
+    }
+    
+    func getMethodSignature() -> String {
+        switch self.method {
+        case .LeagueById, .ChallengerByQueue, .MasterByQueue:
+            return "League"
+        case .PositionById:
+            return "PositionById-\(self.service.region.rawValue)"
+        }
+    }
+    
+    func getMethodUrl() -> String {
+        let entrypoint: String = self.service.host
+        let commonPath: String = "https://\(entrypoint)\(MethodPaths.League.rawValue)/\(Version.RiotAPI)"
+        switch self.method {
+        case .ChallengerByQueue(let queue):
+            return "\(commonPath)/challengerleagues/by-queue/\(queue.type.rawValue)"
+        case .LeagueById(let id):
+            return "\(commonPath)/leagues/\(id)"
+        case .MasterByQueue(let queue):
+            return "\(commonPath)/masterleagues/by-queue/\(queue.type.rawValue)"
+        case .PositionById(let id):
+            return "\(commonPath)/positions/by-summoner/\(id)"
+        }
+    }
+}
