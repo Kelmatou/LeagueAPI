14093e553e550f224c2e4b5f0b200d9c804efe4c
Antoine CLOP <antoine.clop@epita.fr>
2018-08-09 01:38:01 +0200

    [PRIMITIVE CAST] Adding cast method in PrimitiveType protocol

--- LeagueAPI/LeagueAPI/DataAccess/RawRequester.swift
+++ LeagueAPI/LeagueAPI/DataAccess/RawRequester.swift
@@ -12,7 +12,7 @@ internal class RawRequester: DataRequester {
     
     func request<DataType: Decodable>(accessMethod: RESTRequester.AccessMethod, methodUrl: String, headers: [String : String], body: Data?, handler: @escaping (DataType?, RESTRequester.Headers?, String?) -> Void) {
         RESTRequester.request(accessMethod, url: methodUrl, headers: headers, body: body) { (data, headers, error) in
-            if let result = data as? DataType {
+            if let result = PrimitiveTypeCheck.cast(from: data, into: DataType.self) as? DataType {
                 handler(result, headers, error)
             }
             else if let error = error {
--- LeagueAPI/LeagueAPI/Utils/PrimitiveType.swift
+++ LeagueAPI/LeagueAPI/Utils/PrimitiveType.swift
@@ -8,10 +8,27 @@
 
 import Foundation
 
-protocol PrimitiveType {}
-extension Int64: PrimitiveType {}
-extension String: PrimitiveType {}
-extension Int: PrimitiveType {}
+protocol PrimitiveType {
+    static func cast(from data: Data) -> Any?
+}
+
+extension Int64: PrimitiveType {
+    static func cast(from data: Data) -> Any? {
+        guard let dataStr = String.cast(from: data) as? String else { return nil }
+        return Int64(dataStr)
+    }
+}
+extension String: PrimitiveType {
+    static func cast(from data: Data) -> Any? {
+        return String(data: data, encoding: .utf8)
+    }
+}
+extension Int: PrimitiveType {
+    static func cast(from data: Data) -> Any? {
+        guard let dataStr = String.cast(from: data) as? String else { return nil }
+        return Int(dataStr)
+    }
+}
 
 protocol ArrayProtocol {
     static func type() -> Array<Any>.Element.Type
@@ -32,6 +49,16 @@ internal class PrimitiveTypeCheck {
         }
         else {
             return type is PrimitiveType.Type
+        }
+    }
+    
+    public static func cast(from data: Data?, into type: Any.Type) -> Any? {
+        guard let data = data else { return nil }
+        if let primitiveType = type as? PrimitiveType.Type {
+            return primitiveType.cast(from: data)
+        }
+        else {
+            return data as Any
         }
     }
 }
