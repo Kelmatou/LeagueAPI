868938875b79c1a7c2e85fc9551c7e4fda73b758
Antoine CLOP <antoine.clop@epita.fr>
2018-08-25 22:20:16 +0200

    [RUNES] Adding rune methods to LeagueAPI framework

---
+++ LeagueAPI/LeagueAPI/Business/DataDragonBusiness/DataDragonRuneBusiness.swift
@@ -0,0 +1,9 @@
+//
+//  DataDragonRuneBusiness.swift
+//  LeagueAPI
+//
+//  Created by Antoine Clop on 8/25/18.
+//  Copyright © 2018 Antoine Clop. All rights reserved.
+//
+
+import Foundation
---
+++ LeagueAPI/LeagueAPI/Types/RunePathId.swift
@@ -0,0 +1,9 @@
+//
+//  RunePathId.swift
+//  LeagueAPI
+//
+//  Created by Antoine Clop on 8/25/18.
+//  Copyright © 2018 Antoine Clop. All rights reserved.
+//
+
+import Foundation
