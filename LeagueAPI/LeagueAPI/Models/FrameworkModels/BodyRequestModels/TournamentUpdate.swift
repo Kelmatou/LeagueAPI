ba981c9d9ee3e9aefe28aa74d4474913babfb7c9
Antoine CLOP <antoine.clop@epita.fr>
2018-08-28 17:17:33 +0200

    [TOURNAMENT] Creating LeagueMethod for official tournament methods

---
+++ LeagueAPI/LeagueAPI/Models/FrameworkModels/BodyRequestModels/TournamentUpdate.swift
@@ -0,0 +1,41 @@
+//
+//  TournamentUpdate.swift
+//  LeagueAPI
+//
+//  Created by Antoine Clop on 8/28/18.
+//  Copyright © 2018 Antoine Clop. All rights reserved.
+//
+
+import Foundation
+
+public class TournamentUpdate: Encodable {
+    
+    public var spectatorType: SpectatorType
+    public var pickType: PickType
+    public var allowedSummonerIds: [SummonerId]?
+    public var mapType: MapType
+    
+    enum CodingKeys: String, CodingKey {
+        case spectatorType = "spectatorType"
+        case pickType = "pickType"
+        case allowedSummonerIds = "allowedSummonerIds"
+        case mapType = "mapType"
+    }
+    
+    public init(spectatorType: SpectatorType, pickType: PickType, allowedSummonerIds: [SummonerId]? = nil, mapType: MapType) {
+        self.spectatorType = spectatorType
+        self.pickType = pickType
+        self.allowedSummonerIds = allowedSummonerIds
+        self.mapType = mapType
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.spectatorType.rawValue, forKey: .spectatorType)
+        try container.encode(self.pickType.rawValue, forKey: .pickType)
+        if let allowedSummonerIds = self.allowedSummonerIds {
+            try container.encode(allowedSummonerIds.map { $0.value }, forKey: .allowedSummonerIds)
+        }
+        try container.encode(self.mapType.rawValue, forKey: .mapType)
+    }
+}
---
+++ LeagueAPI/LeagueAPI/Models/FrameworkModels/Methods/TournamentMethod.swift
@@ -0,0 +1,82 @@
+//
+//  TournamentMethod.swift
+//  LeagueAPI
+//
+//  Created by Antoine Clop on 8/28/18.
+//  Copyright © 2018 Antoine Clop. All rights reserved.
+//
+
+import Foundation
+
+internal class TournamentMethod: LeagueMethod {
+    
+    public enum TournamentStubMethods {
+        case CreateCodes(amount: Int?, tournamentId: TournamentId, info: TournamentInfo)
+        case UpdateTournamentInfo(code: TournamentCode, updatedInfo: TournamentUpdate?)
+        case GetTournamentInfo(code: TournamentCode)
+        case EventsByTournamentCode(code: TournamentCode)
+        case CreateProvider(callbackUrl: String, region: TournamentRegion)
+        case CreateTournament(name: String, providerId: ProviderId)
+    }
+    
+    private var host: Endpoint
+    private var method: TournamentStubMethods
+    
+    public init(method: TournamentStubMethods) {
+        self.method = method
+        self.host = .America
+    }
+    
+    public func getAccessMethod() -> RESTRequester.AccessMethod {
+        switch self.method {
+        case .CreateCodes, .CreateProvider, .CreateTournament:
+            return .POST
+        case .GetTournamentInfo, .EventsByTournamentCode:
+            return .GET
+        case .UpdateTournamentInfo:
+            return .PUT
+        }
+    }
+    
+    public func getMethodSignature() -> String {
+        return "Tournament" // Since documentation does not explain Tournament Rate Limit, we create a special queue to count rate limit.
+    }
+    
+    public func getMethodUrl() -> String {
+        let entrypoint: String = self.host.rawValue
+        let commonPath: String = "https://\(entrypoint)\(MethodPaths.Tournament.rawValue)/\(Version.RiotAPI)"
+        switch self.method {
+        case .CreateCodes(let amount, let tournamentId, _):
+            return "\(commonPath)/codes?tournamentId=\(tournamentId)&count=\(amount ?? 1)"
+        case .UpdateTournamentInfo(let code, _), .GetTournamentInfo(let code):
+            return "\(commonPath)/codes/\(code)"
+        case .EventsByTournamentCode(let code):
+            return "\(commonPath)/lobby-events/by-code/\(code)"
+        case .CreateProvider:
+            return "\(commonPath)/providers"
+        case .CreateTournament:
+            return "\(commonPath)/tournaments"
+        }
+    }
+    
+    func getMethodBody() -> Data? {
+        switch self.method {
+        case .CreateCodes(_, _, let info):
+            return ObjectMapper.encode(info)
+        case .UpdateTournamentInfo(_, let updatedInfo):
+            return ObjectMapper.encode(updatedInfo)
+        case .EventsByTournamentCode, .GetTournamentInfo:
+            return nil
+        case .CreateProvider(let callbackUrl, let region):
+            let tournamentProvider: TournamentProvider = TournamentProvider(region: region.region.rawValue, url: callbackUrl)
+            return ObjectMapper.encode(tournamentProvider)
+        case .CreateTournament(let name, let providerId):
+            let tournamentInitializer: TournamentInitializer = TournamentInitializer(name: name, providerId: providerId)
+            return ObjectMapper.encode(tournamentInitializer)
+        }
+    }
+    
+    func getWarningMessage() -> String? {
+        return nil
+    }
+}
