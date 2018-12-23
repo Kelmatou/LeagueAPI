cb25b5d106e058176ebc873f9ea72c4b65c32ab3
Antoine CLOP <antoine.clop@epita.fr>
2018-12-23 12:42:50 +0100

    [LOG] Requesting and Response info logs are now debug logs

--- LeagueAPI/LeagueAPI/Utils/RESTRequester.swift
+++ LeagueAPI/LeagueAPI/Utils/RESTRequester.swift
@@ -52,11 +52,11 @@ internal class RESTRequester {
                 }
                 let allHeaders: Headers? = httpResponse?.allHeaderFields
                 let responseCode: HttpResponseCode = HttpResponseCode(httpResponse?.statusCode ?? -1)
-                Logger.info("Server responsed with code \(responseCode.codeValue) (\(responseCode.errorMessage() ?? "No description"))")
+                Logger.debug("Server responsed with code \(responseCode.codeValue) (\(responseCode.errorMessage() ?? "No description"))")
                 let errorValue: String? = error?.localizedDescription ?? responseCode.errorMessage()
                 handler(data, responseCode, allHeaders, errorValue)
             }
-            Logger.info("Requesting: \(url)")
+            Logger.debug("Requesting: \(url)")
             task.resume()
         }
         else {
