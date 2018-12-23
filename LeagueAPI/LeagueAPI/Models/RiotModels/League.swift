3aeabe5ac9492c764d86a95eb744974ef5410425
Antoine CLOP <antoine.clop@epita.fr>
2018-12-23 16:54:33 +0100

    [LEAGUE] Removing deprecated fields in LeagueInfo

--- LeagueAPI/LeagueAPI/Models/RiotModels/League.swift
+++ LeagueAPI/LeagueAPI/Models/RiotModels/League.swift
@@ -11,33 +11,25 @@ import Foundation
 public class League: Decodable {
     
     public var leagueId: LeagueId
-    public var name: String
     public var tier: RankedTier
-    public var queue: Queue
     public var leagueInfoList: [LeagueInfo]
     
     enum CodingKeys: String, CodingKey {
         case leagueId = "leagueId"
-        case name = "name"
         case tier = "tier"
-        case queue = "queue"
         case leagueInfoList = "entries"
     }
     
-    public init(leagueId: LeagueId, name: String, tier: RankedTier, queue: Queue, leagueInfoList: [LeagueInfo]) {
+    public init(leagueId: LeagueId, tier: RankedTier, leagueInfoList: [LeagueInfo]) {
         self.leagueId = leagueId
-        self.name = name
         self.tier = tier
-        self.queue = queue
         self.leagueInfoList = leagueInfoList
     }
     
     public required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.leagueId = try LeagueId(container.decode(String.self, forKey: .leagueId))
-        self.name = try container.decode(String.self, forKey: .name)
         self.tier = try RankedTier(container.decode(String.self, forKey: .tier))
-        self.queue = try Queue(container.decode(String.self, forKey: .queue))
         self.leagueInfoList = try container.decode([LeagueInfo].self, forKey: .leagueInfoList)
     }
 }
