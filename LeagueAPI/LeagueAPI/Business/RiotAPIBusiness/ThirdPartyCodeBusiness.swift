95d686d0d685c84f259ab81c05f147b358276668
Antoine Clop <antoine.clop@kanopy.com>
2019-04-28 18:02:57 -0700

    [VERIFICATION] if player enters "test", Riot API returns "\"test\"". LeagueAPI now return exact same string as the user entered

--- LeagueAPI/LeagueAPI/Business/RiotAPIBusiness/ThirdPartyCodeBusiness.swift
+++ LeagueAPI/LeagueAPI/Business/RiotAPIBusiness/ThirdPartyCodeBusiness.swift
@@ -13,8 +13,15 @@ internal class ThirdPartyCodeBusiness {
     public static func getVerificationCode(method: ThirdPartyCodeMethod.ThirdPartyCodeMethods, region: Region, key: APIKey, handler: @escaping (String?, String?) -> Void) {
         let thirdPartyCodeMethod: ThirdPartyCodeMethod = ThirdPartyCodeMethod(method: method, region: region)
         let thirdPartyCodeBusiness: APIBusiness = APIBusiness(key: key, method: thirdPartyCodeMethod)
-        thirdPartyCodeBusiness.request() { (code, error) in
-            handler(code, error)
+        thirdPartyCodeBusiness.request() { (code: String?, error: String?) in
+            // Riot API returns verification code into double quotes, but we want to return the exact string entered by the player
+            let cleanCode: String? = ThirdPartyCodeBusiness.removeExtraDoubleQuotes(code)
+            handler(cleanCode, error)
         }
+    }
+    
+    internal static func removeExtraDoubleQuotes(_ receivedCode: String?) -> String? {
+        guard let receivedCodeUnwrapped = receivedCode, receivedCodeUnwrapped.count >= 2 else { return receivedCode }
+        return receivedCodeUnwrapped.substring(startIndex: 1, length: receivedCodeUnwrapped.count - 2)
     }
 }
