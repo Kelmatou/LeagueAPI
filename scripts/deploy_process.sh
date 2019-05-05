91016ac46641e01524a1c36169be891dda620bee
Antoine Clop <antoine.clop@kanopy.com>
2019-05-05 12:52:54 -0700

    [TRAVIS] Cleaning CI pipeline

--- scripts/deploy_process.sh
+++ scripts/deploy_process.sh
@@ -1,36 +1,31 @@
 #!/usr/bin/env bash
 
-OLD_GIT_TAG="$(git tag -l --contains HEAD)"
-GIT_TAG="$TRAVIS_TAG"
-echo "OLD_GIT_TAG $OLD_GIT_TAG"
-echo "GIT_TAG $GIT_TAG"
-
-echo "Getting git tag"
-if [ "$GIT_TAG" != "" ]; then
-  echo "   Git tag found ($GIT_TAG)"
+echo "Getting tag"
+if [ "$TRAVIS_TAG" != "" ]; then
+  echo "   Tag found ($TRAVIS_TAG)"
 else
-  echo "   Git tag not found"
+  echo "   Tag not found"
   exit 1
 fi
 
 EXIT_CODE=0
 
 echo "Step 1/3 Before deploy"
-./scripts/before_deploy.sh "$GIT_TAG"
+./scripts/before_deploy.sh "$TRAVIS_TAG"
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
   exit $EXIT_CODE
 fi
 
 echo "Step 2/3 Deploy"
-./scripts/deploy.sh "$GIT_TAG"
+./scripts/deploy.sh "$TRAVIS_TAG"
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
   exit $EXIT_CODE
 fi
 
 echo "Step 3/3 After deploy"
-./scripts/after_deploy.sh "$GIT_TAG"
+./scripts/after_deploy.sh "$TRAVIS_TAG"
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
   exit $EXIT_CODE
