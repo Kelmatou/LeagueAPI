e694e944aabf27d6f574d505911cf5ba09648cd2
Bilel <bilel@powder.gg>
2022-02-08 00:26:10 +0100

    Fix Ranked Entry Data

--- LeagueAPI/LeagueAPI/Models/RiotModels/RankedEntry.swift
+++ LeagueAPI/LeagueAPI/Models/RiotModels/RankedEntry.swift
@@ -11,65 +11,24 @@ import Foundation
 public class RankedEntry: Decodable {
     
     public var leagueId: LeagueId
-    public var summonerId: SummonerId
-    public var summonerName: String
-    public var rank: String
-    public var leaguePoints: Int
-    public var series: Series?
-    public var wins: Int
-    public var losses: Int
-    public var hotStreak: Bool
-    public var veteran: Bool
-    public var freshBlood: Bool
-    public var inactive: Bool
+    public var leagueInfo: LeagueInfo
     public var queue: Queue
     
     enum CodingKeys: String, CodingKey {
-        case leagueId = "leagueId"
+        case leagueId = "leagueID"
         case queue = "queueType"
-        case summonerId = "summonerId"
-        case summonerName = "summonerName"
-        case rank = "rank"
-        case leaguePoints = "leaguePoints"
-        case series = "miniSeries"
-        case wins = "wins"
-        case losses = "losses"
-        case hotStreak = "hotStreak"
-        case veteran = "veteran"
-        case freshBlood = "freshBlood"
-        case inactive = "inactive"
     }
     
-    public init(leagueId: LeagueId, summonerId: SummonerId, summonerName: String, rank: String, leaguePoints: Int, series: Series?, wins: Int, losses: Int, hotStreak: Bool, veteran: Bool, freshBlood: Bool, inactive: Bool, queue: Queue) {
+    public init(leagueId: LeagueId, leagueName: String, leagueInfo: LeagueInfo, queue: Queue) {
         self.leagueId = leagueId
+        self.leagueInfo = leagueInfo
         self.queue = queue
-        self.summonerId = summonerId
-        self.summonerName = summonerName
-        self.rank = rank
-        self.leaguePoints = leaguePoints
-        self.series = series
-        self.wins = wins
-        self.losses = losses
-        self.hotStreak = hotStreak
-        self.veteran = veteran
-        self.freshBlood = freshBlood
-        self.inactive = inactive
     }
     
     public required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.leagueId = try LeagueId(container.decode(String.self, forKey: .leagueId))
+        self.leagueInfo = try LeagueInfo(from: decoder)
         self.queue = try Queue(container.decode(String.self, forKey: .queue))
-        self.summonerId = try SummonerId(container.decode(String.self, forKey: .summonerId))
-        self.summonerName = try container.decode(String.self, forKey: .summonerName)
-        self.rank = try container.decode(String.self, forKey: .rank)
-        self.leaguePoints = try container.decode(Int.self, forKey: .leaguePoints)
-        self.series = try? container.decode(Series.self, forKey: .series)
-        self.wins = try container.decode(Int.self, forKey: .wins)
-        self.losses = try container.decode(Int.self, forKey: .losses)
-        self.hotStreak = try container.decode(Bool.self, forKey: .hotStreak)
-        self.veteran = try container.decode(Bool.self, forKey: .veteran)
-        self.freshBlood = try container.decode(Bool.self, forKey: .freshBlood)
-        self.inactive = try container.decode(Bool.self, forKey: .inactive)
     }
 }
