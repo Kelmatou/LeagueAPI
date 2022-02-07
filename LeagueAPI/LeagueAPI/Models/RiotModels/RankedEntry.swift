558cd69b903165998c3b2354f293aafe7831bf57
Bilel <bilel@powder.gg>
2022-02-07 23:33:01 +0100

    fix

--- LeagueAPI/LeagueAPI/Models/RiotModels/RankedEntry.swift
+++ LeagueAPI/LeagueAPI/Models/RiotModels/RankedEntry.swift
@@ -10,19 +10,19 @@ import Foundation
 
 public class RankedEntry: Decodable {
     
-    public var tier: RankedTier
+    //public var tier: RankedTier
     public var leagueId: LeagueId
     public var leagueInfo: LeagueInfo
     public var queue: Queue
     
     enum CodingKeys: String, CodingKey {
-        case tier = "tier"
+        //case tier = "tier"
         case leagueId = "leagueId"
         case queue = "queueType"
     }
     
-    public init(tier: RankedTier, leagueId: LeagueId, leagueName: String, leagueInfo: LeagueInfo, queue: Queue) {
-        self.tier = tier
+    public init(leagueId: LeagueId, leagueName: String, leagueInfo: LeagueInfo, queue: Queue) {
+        //self.tier = tier
         self.leagueId = leagueId
         self.leagueInfo = leagueInfo
         self.queue = queue
@@ -30,7 +30,7 @@ public class RankedEntry: Decodable {
     
     public required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.tier = try RankedTier(container.decode(String.self, forKey: .tier))
+        //self.tier = try RankedTier(container.decode(String.self, forKey: .tier))
         self.leagueId = try LeagueId(container.decode(String.self, forKey: .leagueId))
         self.leagueInfo = try LeagueInfo(from: decoder)
         self.queue = try Queue(container.decode(String.self, forKey: .queue))
